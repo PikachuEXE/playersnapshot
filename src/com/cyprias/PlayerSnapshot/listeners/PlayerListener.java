13ecbb759bbe6dfe1bf7a68a57889bea2fd1f8cc
Cyprias <Cyprias@gmail.com>
2014-05-19 16:30:10 -0600

    Added options to take screenshot upon world enter and world leave. Leave on by default.

--- src/com/cyprias/PlayerSnapshot/listeners/PlayerListener.java
+++ src/com/cyprias/PlayerSnapshot/listeners/PlayerListener.java
@@ -2,19 +2,17 @@ package com.cyprias.PlayerSnapshot.listeners;
 
 import java.io.File;
 import java.io.IOException;
-import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
-
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 import com.cyprias.PlayerSnapshot.Logger;
 import com.cyprias.PlayerSnapshot.Plugin;
 import com.cyprias.PlayerSnapshot.commands.RestoreCommand;
@@ -22,6 +20,54 @@ import com.cyprias.PlayerSnapshot.configuration.Config;
 
 public class PlayerListener implements Listener {
 
+	@EventHandler
+	public void onPlayerTeleportEvent(PlayerTeleportEvent e) throws Exception {
+		Player p = e.getPlayer();
+		if (e.getFrom().getWorld().getName() != e.getTo().getWorld().getName())
+		{
+			// Player is leaving their previous world.
+			
+			if (Config.getBoolean("event-snapshots.world-leave")) {
+				if (p.hasPermission("ps.snapshot.world-leave")) {
+					SimpleDateFormat ft = new SimpleDateFormat("yyyyMMdd.hhmmss");
+					String date = ft.format(new Date());
+		
+					String snapName = "world_leave-" + date + "-" + Config.getString("properties.default-lifetime");
+		
+					if (Plugin.BackupPlayer(p, snapName) != null)
+						Logger.info("Created snapshot (" + snapName + ") for " + p.getName() + ".");
+				}
+			}
+		
+		}
+	//	Logger.debug("PlayerTeleportEvent " + p.getName() + " from: " + e.getFrom().getWorld().getName() + ", to: " + e.getTo().getWorld().getName());
+	}
+	
+	/*
+	@EventHandler
+	public void onPlayerMoveEvent(PlayerMoveEvent e) throws Exception {
+		Player p = e.getPlayer();
+		Logger.debug("PlayerMoveEvent " + p.getName() + " from: " + e.getFrom().getWorld().getName() + ", to: " + e.getTo().getWorld().getName());
+	}
+	*/
+	
+	@EventHandler
+	public void onPlayerChangedWorldEvent(PlayerChangedWorldEvent e) throws Exception {
+		Player p = e.getPlayer();
+
+		if (Config.getBoolean("event-snapshots.world-enter")) {
+			if (p.hasPermission("ps.snapshot.world-enter")) {
+				SimpleDateFormat ft = new SimpleDateFormat("yyyyMMdd.hhmmss");
+				String date = ft.format(new Date());
+	
+				String snapName = "world_enter-" + date + "-" + Config.getString("properties.default-lifetime");
+	
+				if (Plugin.BackupPlayer(p, snapName) != null)
+					Logger.info("Created snapshot (" + snapName + ") for " + p.getName() + ".");
+			}
+		}
+	}
+	
 	@EventHandler
 	public void onPlayerDeathEvent(PlayerDeathEvent e) throws Exception {
 
